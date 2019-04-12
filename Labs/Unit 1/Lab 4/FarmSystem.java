81511fe5a6e2fc4b51b364fd48792b8db7218063
Jhony Naranjo <49105428+jhonyagus@users.noreply.github.com>
2019-04-11 20:09:04 -0500

    Create FarmSystem.java

---
+++ Labs/Unit 1/Lab 4/FarmSystem.java
@@ -0,0 +1,14 @@
+import model.Egg;
+
+public class FarmSystem{
+		
+		public static void main(String args[]){
+			
+			Egg egg[]=new Egg[5];
+			egg[0]= new Egg();
+			egg[0].setId(10);
+			System.out.println(egg[0].getId());
+			egg[0].id =8;
+			
+		}
+}
