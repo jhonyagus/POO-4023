bcde2f9ffe40f09fcbeb298dd922b1b1dc723e5d
Jhony Naranjo <49105428+jhonyagus@users.noreply.github.com>
2019-04-11 20:10:34 -0500

    Classes and Objects

---
+++ Labs/Unit 1/Lab 4/model/Chicken.java
@@ -0,0 +1,36 @@
+public class Chicken{
+	private int id;
+	private String name;
+	private String color;
+	private int age;
+	private boolean molting;
+	
+	public void doStuff(int forTime){
+		//to do
+    }
+	
+	private void cluck(){
+		// to do
+	}
+	
+	private void wander(){
+		// to do
+	}
+	
+	private void eat(){
+		// to do
+	}
+	
+	private void drink(){
+		// to do
+	}
+	
+	private void poop(){
+		// to do
+	}
+	
+	private Egg IayAnEgg(){
+		Egg egg = new Egg();
+		return egg;
+	}
+}
---
+++ Labs/Unit 1/Lab 4/model/Coop.java
@@ -0,0 +1,20 @@
+public class Coop{
+	private int id;
+	private Chicken[] chickens;
+	
+	public void add(Chicken chicken){
+		//to do
+	}
+	
+	public void remove(int chickenld){
+		//to do
+	}
+	
+	public void resetIteration(){
+	//to do
+	}
+	
+	public Chicken next(){
+	 return null;
+	}
+}
