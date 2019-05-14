55cd7e9be900321a2511b86af0e4464d9d4ada55
jhonyagus <jhonyagus@hotmail.com>
2019-05-13 21:02:16 -0500

    Add Lab10

---
+++ Labs/Unit 2/Lab10Interfaces/src/ec/edu/espe/lab7oopfundamentals/inheritance/Lion.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.lab7oopfundamentals.inheritance;
+
+/**
+ *
+ * @author Jhony Naranjo
+ */
+public class Lion extends Feline{
+    private boolean mane;
+    
+   String inf = "";
+    @Override
+    public String getData() {
+        inf = "\n{\n \t \"Mane\" : \" "+mane+" \",\n \t \"Scary\" : \" "+isScary()+" \",\n \t \"Fur Thickness\" : \" "+getFurThickness()+" \",\n \t \"Number of Legs\" : \" "+getNumberOfLegs()+" \",\n \t \"Name\" : \" "+getName()+" \" \n}";
+        return inf;
+    }
+
+    public Lion(boolean mane, boolean scary, int furThickness, int numberOfLegs, String name) {
+        super(scary, furThickness, numberOfLegs, name);
+        this.mane = mane;
+    }
+
+    @Override
+    public String toString() {
+        return getData();
+    }
+    
+    
+    
+    /**
+     * @return the mane
+     */
+    public boolean isMane() {
+        return mane;
+    }
+
+    /**
+     * @param mane the mane to set
+     */
+    public void setMane(boolean mane) {
+        this.mane = mane;
+    }
+    
+}
---
+++ Labs/Unit 2/Lab10Interfaces/src/ec/edu/espe/lab7oopfundamentals/interfaces/Horse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.lab7oopfundamentals.interfaces;
+
+/**
+ *
+ * @author Jhony Naranjo
+ */
+public class Horse extends WildAnimal implements Pet, Animal{
+
+    @Override
+    public boolean walkThePet(int timeInSeconds, int distance) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
