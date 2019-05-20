033874ad80f30506a2dddba3b847c229e3ee7693
jhonyagus <jhonyagus@hotmail.com>
2019-05-19 20:25:05 -0500

    Add Lab13

---
+++ Labs/Unit 2/Lab13Library/Fundaments/src/ec/edu/espe/lab7oopfundamentals/interfaces/Animal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.lab7oopfundamentals.interfaces;
+
+/**
+ *
+ * @author Jhony Naranjo
+ */
+public interface Animal {
+    public void run();
+    public void sound();
+    public void sleep(int time); 
+    
+}
